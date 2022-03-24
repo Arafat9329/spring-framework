095416dd4ec0ccc3b8746f3b9173e1689b206013
Arafat9329 <68973655+Arafat9329@users.noreply.github.com>
2022-03-23 22:54:20 -0400

    stereotype_annotation

---
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/stereotype_annotation/Java.java
@@ -0,0 +1,11 @@
+package com.cydeo.stereotype_annotation;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Java {
+
+    public void getTeachingHours(){
+        System.out.println("Total: Java teaching hours : 150");
+    }
+}
