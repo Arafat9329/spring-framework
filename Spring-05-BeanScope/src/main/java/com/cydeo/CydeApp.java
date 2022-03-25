fbede2fced4919324da618e0c670e4f431284d2a
Arafat9329 <68973655+Arafat9329@users.noreply.github.com>
2022-03-25 02:01:16 -0400

    BeanScope

--- Spring-05-BeanScope/src/main/java/com/cydeo/CydeApp.java
+++ Spring-05-BeanScope/src/main/java/com/cydeo/CydeApp.java
@@ -9,13 +9,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class CydeApp {
 
     public static void main(String[] args) {
-        Comment comment = new Comment();
-        comment.setAuthor("Johson");
-        comment.setText("Spring Framework");
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        CommentService commentService = context.getBean(CommentService.class);
-        commentService.publishComment(comment);
     }
 }
