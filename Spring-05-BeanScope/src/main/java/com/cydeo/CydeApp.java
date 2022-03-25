991ba1a90cf5959c2ba74361868909d84780f57a
Arafat9329 <68973655+Arafat9329@users.noreply.github.com>
2022-03-25 02:07:08 -0400

    @Scope("prototype")

--- Spring-05-BeanScope/src/main/java/com/cydeo/CydeApp.java
+++ Spring-05-BeanScope/src/main/java/com/cydeo/CydeApp.java
@@ -9,7 +9,18 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class CydeApp {
 
     public static void main(String[] args) {
+        Comment comment = new Comment();
+        comment.setAuthor("Johson");
+        comment.setText("Spring Framework");
 
+        ApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
+        CommentService cs1 = context.getBean(CommentService.class);
+        CommentService cs2 = context.getBean(CommentService.class);
+
+        System.out.println(cs1);
+        System.out.println(cs2);
+
+        System.out.println(cs1==cs2);//Beam will only create one object
     }
 }
