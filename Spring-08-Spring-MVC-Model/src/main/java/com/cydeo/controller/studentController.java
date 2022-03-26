73c89c46491b5d2576d504e37cbbf8c08bfc847d
Arafat9329 <68973655+Arafat9329@users.noreply.github.com>
2022-03-26 16:33:59 -0400

    MVC Model

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/studentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/studentController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 @Controller//object -- sprint boot
 public class studentController {
 
@@ -14,6 +18,17 @@ public class studentController {
         model.addAttribute("name","Cydeo");
         model.addAttribute("course","MVC");
 
+        //random number
+        int studentID = new Random().nextInt(1000);
+        model.addAttribute("randomNumber",studentID);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(7);
+        numbers.add(10);
+        numbers.add(4);
+        model.addAttribute("numbers",numbers);
 
         return "/student/welcome";// HTML location, don't need .html because its templates file
     }
