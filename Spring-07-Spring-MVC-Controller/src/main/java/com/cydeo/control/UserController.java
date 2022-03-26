4161d38d3c10871a89880688b0d0ef1d72d7e4f5
Arafat9329 <68973655+Arafat9329@users.noreply.github.com>
2022-03-26 15:09:22 -0400

    multi controller

--- Spring-07-Spring-MVC-Controller/src/main/java/com/cydeo/control/UserController.java
+++ Spring-07-Spring-MVC-Controller/src/main/java/com/cydeo/control/UserController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class UserController {
 
-
     @RequestMapping("/user")// end point of URL
     public String user (){
+        String a = "apple";
         return "/user/userinfo.html";// go to folder static and look over there
     }
 
