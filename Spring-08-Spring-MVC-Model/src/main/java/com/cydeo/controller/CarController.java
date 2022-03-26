943fd73deadc03030ab933572133a0684ade9a3b
Arafat9329 <68973655+Arafat9329@users.noreply.github.com>
2022-03-26 17:35:21 -0400

    multiple @PathVariable Integer year,

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
@@ -2,6 +2,7 @@ package com.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,6 +17,17 @@ public class CarController {
 
         System.out.println(make);
         System.out.println(year);
+        model.addAttribute("make",make);
+        model.addAttribute("year",year);
+
+        return "car/car-info";
+    }
+
+    @RequestMapping("/info/{make}/{year}")    //http://localhost:8080/car/info/honda/year
+    public String getCarInfo(@PathVariable String make,
+                             @PathVariable Integer year,
+                             Model model){
+
         model.addAttribute("make",make);
         model.addAttribute("year",year);
 
