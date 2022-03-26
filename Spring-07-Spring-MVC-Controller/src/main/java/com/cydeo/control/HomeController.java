422987ee2b1d745dc9d7f1c5d1f205d34709c797
Arafat9329 <68973655+Arafat9329@users.noreply.github.com>
2022-03-26 14:50:55 -0400

    @RequestMapping

--- Spring-07-Spring-MVC-Controller/src/main/java/com/cydeo/control/HomeController.java
+++ Spring-07-Spring-MVC-Controller/src/main/java/com/cydeo/control/HomeController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
+    @RequestMapping("/home")//Use @RequestMapping annotation to associate the action with an HTTP request path
     public String home (){
-        return "home.html";
+        return "home.html";//Return the HTML document name that contains the details we want the browser to display
     }
 
     @RequestMapping("/welcome")
@@ -16,4 +16,8 @@ public class HomeController {
         return "welcome.html";
     }
 
+    @RequestMapping()// if you don't put anything here, as default it will put  ("/")
+    public String home3 (){
+        return "welcome.html";
+    }
 }
