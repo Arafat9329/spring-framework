b1493e83bec426442936ab35f2e92857286429e5
Arafat9329 <68973655+Arafat9329@users.noreply.github.com>
2022-03-27 21:02:09 -0400

    Submit from get from info redirect info

--- Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -3,13 +3,16 @@ package com.cydeo.controller;
 import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/student") //http://localhost:8080/student
+@RequestMapping("/student") //http://localhost:8080/student// in class level we can't do this
 public class StudentController {
 
-    @RequestMapping("/register")//http://localhost:8080/student/register
+//    @RequestMapping(value = "/register", method = RequestMethod.GET)//http://localhost:8080/student/register
+    @GetMapping(value = "/register")//http://localhost:8080/student/register  this is same as above, sprint 4.3
     public String register(Model model){
         model.addAttribute("students", DataGenerator.createdStudent());
         return "student/register";
